4a408e6f976b189d9baa4ffa4d561ef3c76395d2
dawkinspw <dawkinspw@gmail.com>
2021-04-29 19:47:29 -0400

    if elif example

---
+++ 2-FlowControl/if_elif_example.py
@@ -0,0 +1,10 @@
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+    print('You\' not Alice, kiddo.')
+elif age > 200:
+    print('Unlike you, Alice is not an undead, immportal vampire.')
+elif age > 100:
+    print('You\' not Alice, grannie.')
