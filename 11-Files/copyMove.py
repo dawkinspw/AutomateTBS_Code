8fde775543adbc310b963e675559775c61d97a1a
Patrick Dawkins <patrickdawkins@Patricks-MacBook-Air.local>
2021-05-21 18:15:19 -0400

    copy/move example

---
+++ 11-Files/copyMove.py
@@ -0,0 +1,3 @@
+import shutil
+shutil.copy('mydata.db', 'mydata2.db')
+shutil.move('mydata.db', 'mydata3.db')
--- 11-Files/fileExamples.py
+++ 11-Files/fileExamples.py
@@ -1,7 +1,14 @@
-testFile = open('.\\testTex2.txt', 'a')
+import os, shelve
+
+
+testFile = open('testTex2.txt', 'a')
 testFile.write('Testing abc\n')
 
+shelfFile = shelve.open('mydata')
+shelfFile['cats'] = ['one', 'two', 'three']
 
+print(shelfFile['cats'])
+shelfFile.close()
 
 # content = testFile.read()
 
